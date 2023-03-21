56e79fa4324a860b570c9e7eb085c045c5768e63
Breland Norwood <brelandnorwood@gmail.com>
2023-03-21 20:05:04 +0000

    working on mars rover

--- marsrover/assets/js/scripts.js
+++ marsrover/assets/js/scripts.js
@@ -10,26 +10,83 @@ async function GoSearch() {
     // If all of the form elements are valid, the get the form values
     if (form.valid()) {
         
-        var BaseCurrency = document.getElementById("BaseCurrency").value;
-        var ConvertCurrency = document.getElementById("ConvertCurrency").value;
-        var apiKey = "5ePDCCpwAMU6RMIcigYMSSLOA5Yga2HG"
-        var FromDate = document.getElementById("FromDate").value;
-        var ToDate = document.getElementById("ToDate").value;
+        var photoDate = document.getElementById("photoDate").value;
+        var apiKey = "SEhoXqBiy5mxt8RJbbHJXbqshoIpQVUb6FcXuOVP";
 
-   
-            
- 
+        var roverChoice;
+        if (document.getElementById("Curiosity").checked) {
+            roverChoice = document.getElementById("Curiosity").value;
+        }
+        if (document.getElementById("Opportunity").checked) {
+            roverChoice = document.getElementById("Opportunity").value;
+        }
+        if (document.getElementById("Spirit").checked) {
+            roverChoice = document.getElementById("Spirit").value;
+        }
+
+        var myURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverChoice + "/photos?earth_date=" + photoDate + "&page=1&api_key=" + apiKey;
+        var myMethod = "GET";
+
+        $(document).ready(function () {
+
+            $.ajax({
+                method: myMethod,
+                url: myURL
+            })
+
+                .done(function (msg) {
+
+                    var numphotos = msg.photos.length;
+                    if (numphotos > 0) {
+                        for (var i = 0; i < 25; i++) {
+                            if (i < numphotos) {
+                                document.getElementById("mars1" + i).src = msg.photos[i].img_src;
+                                document.getElementById("mars1" + i).title = msg.photos[i].camera.full_name;
+                                document.getElementById("rover1" + i).href = msg.photos[i].img_src;
+                                document.getElementById("text1").innerHTML = msg.photos.length + " photos found";
+                                document.getElementById("text2").innerHTML = "Click a photo to display full size";
+                            }
+
+                            else {
+                                document.getElementById("rover1" + i).src = "#";
+                                document.getElementById("anchor" + i).href = "#";
+                                document.getElementById("mars1" + i).style.display = "none";
+                            }
+                        }
+                    }
+                })
+
+                .fail(function (msg) {
+                    alert("Rover Not Found - Status: " + msg.status);
+                });
+        });
     }
 }
 
-function ClearForm() {
-    document.getElementById("rover").value = "";
-    
-    document.getElementById("PicDate").value = "";
-    
-    document.getElementById("PicDateError").innerHTML = "";
-    
-    
-   
-    
+function clearform() {
+    for (var i = 0; i < 25; i++) {
+        document.getElementById("Curiosity").checked = false;
+        document.getElementById("Opportunity").checked = false;
+        document.getElementById("Spirit").checked = false;
+        document.getElementById("photoDate").value = "";
+        document.getElementById("roverError").innerHTML = "";
+        document.getElementById("photoDateError").innerHTML = "";
+        document.getElementById("rover1" + i).src = "#";
+        document.getElementById("anchor" + i).href = "";
+        document.getElementById("mars1" + i).title = "";
+        document.getElementById("text1").innerHTML = "";
+        document.getElementById("text2").innerHTML = "";
+    }
+}
+
+function getCuriosity() {
+    document.getElementById("photoDate").value = "2012-08-06";
+}
+
+function getOpportunity() {
+    document.getElementById("photoDate").value = "2004-01-26";
+}
+
+function getSpirit() {
+    document.getElementById("photoDate").value = "2004-01-05";
 }
