cfd409ca1b04ef40e27dc7a5a1539f4b2f95703a
Breland Norwood <brelandnorwood@gmail.com>
2023-04-11 19:38:44 +0000

    added to river

--- buffalo/assets/js/scripts.js
+++ buffalo/assets/js/scripts.js
@@ -37,4 +37,95 @@ var myChart = new Chart(ctx, {
         maintainAspectRatio: true,
     }
 });
+ 
+
+/* Site 1 */
+var dates = [];
+var values = [];
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[1].values[0].value.length
+for (i = 0; i < fLen; i++) {
+    values[i] = msg.value.timeSeries[1].values[0].value[i].value
+	dates[i] = msg.value.timeSeries[1].values[0].value[i].dateTime
+}
+var sitename = msg.value.timeSeries[1].sourceInfo.siteName
+var sitecode = msg.value.timeSeries[1].sourceInfo.siteCode[0].value
+var siteDescription = msg.value.timeSeries[1].variable.variableDescription
+
+var ctx = document.getElementById("chartjs-1");
+
+var myChart = new Chart(ctx, {
+    "type":"line",
+    "data": {
+        "labels": dates,
+        "datasets":[{
+            "data": values,
+            fill: false
+        }]
+    },
+    "options":{ 
+        responsive: false,
+        maintainAspectRatio: true,
+    }
+});
+
+/* Site 2 */
+var dates = [];
+var values = [];
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[2].values[0].value.length
+for (i = 0; i < fLen; i++) {
+    values[i] = msg.value.timeSeries[2].values[0].value[i].value
+	dates[i] = msg.value.timeSeries[2].values[0].value[i].dateTime
+}
+var sitename = msg.value.timeSeries[2].sourceInfo.siteName
+var sitecode = msg.value.timeSeries[2].sourceInfo.siteCode[0].value
+var siteDescription = msg.value.timeSeries[2].variable.variableDescription
+
+var ctx = document.getElementById("chartjs-2");
+
+var myChart = new Chart(ctx, {
+    "type":"line",
+    "data": {
+        "labels": dates,
+        "datasets":[{
+            "data": values,
+            fill: false
+        }]
+    },
+    "options":{ 
+        responsive: false,
+        maintainAspectRatio: true,
+    }
+});
+
+/* Site 3 */
+var dates = [];
+var values = [];
+/* fLen contains the length of the array (number of values) */
+fLen = msg.value.timeSeries[3].values[0].value.length
+for (i = 0; i < fLen; i++) {
+    values[i] = msg.value.timeSeries[3].values[0].value[i].value
+	dates[i] = msg.value.timeSeries[3].values[0].value[i].dateTime
+}
+var sitename = msg.value.timeSeries[3].sourceInfo.siteName
+var sitecode = msg.value.timeSeries[3].sourceInfo.siteCode[0].value
+var siteDescription = msg.value.timeSeries[3].variable.variableDescription
+
+var ctx = document.getElementById("chartjs-3");
+
+var myChart = new Chart(ctx, {
+    "type":"line",
+    "data": {
+        "labels": dates,
+        "datasets":[{
+            "data": values,
+            fill: false
+        }]
+    },
+    "options":{ 
+        responsive: false,
+        maintainAspectRatio: true,
+    }
+});
 }
